148e1eca1fdf5c53ada63fd5588ea20a3f375e32
Jan_Reisiegel <jan.reisiegel@tul.cz>
2024-04-08 15:07:13 +0200

    Add workflow to public on Azure

--- WeatherApp/WeatherApi/Program.cs
+++ WeatherApp/WeatherApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,13 +8,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "WeatherApi",
+        Version = "v1"
+
+    });
+});
 
 var app = builder.Build();
 
+app.UseSwagger();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
     app.UseSwaggerUI();
 }
 
