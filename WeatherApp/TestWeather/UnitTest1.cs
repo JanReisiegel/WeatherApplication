e6d7b40df848b14e5b5f2aded416f29bd1198fcc
Jan_Reisiegel <jan.reisiegel@tul.cz>
2024-04-08 15:30:26 +0200

    First Test

---
+++ WeatherApp/TestWeather/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using WeatherApi.Controllers;
+
+namespace TestWeather
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        WeatherController weatherController = new WeatherController();
+        [TestMethod]
+        public void Test1()
+        {
+            var result = weatherController.Get() as ObjectResult;
+            Assert.AreEqual("Hello World", result.Value);
+        }
+    }
+}
---
+++ WeatherApp/WeatherApi/Controllers/WeatherController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApi.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
