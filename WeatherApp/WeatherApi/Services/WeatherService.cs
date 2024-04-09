168983f238b7823141bb5a2b6779af6ffff2c77e
Jan_Reisiegel <jan.reisiegel@tul.cz>
2024-04-09 23:31:07 +0200

    some changes

--- WeatherApp/WeatherApi/Services/WeatherService.cs
+++ WeatherApp/WeatherApi/Services/WeatherService.cs
@@ -6,8 +6,20 @@ namespace WeatherApi.Services
     {
         private static OpenWeatherMapOptions openWeatherMapOptions = new OpenWeatherMapOptions
         {
-            ApiKey = WeatherConstants.OpenWeatherMapOptions
+            ApiKey = WeatherConstants.OpenWeatherMapOptions,
+            ApiEndpoint = "https://api.openweathermap.org/data/2.5/",
+            Language = "cs",
+            UnitSystem = "metric"
         };
         private static IOpenWeatherMapService openWeatherMapService = new OpenWeatherMapService(openWeatherMapOptions);
+
+        public WeatherService()
+        {
+        }
+
+        public string GetWeather(string city)
+        {
+            var weather = openWeatherMapService.
+        }
     }
 }
